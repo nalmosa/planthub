95f985004c2e930a4a4abb6f225462d5d4507aa5
catdelaney <delan@DESKTOP-PTA0O78.(none)>
2024-04-17 14:25:35 -0700

    yt edits

---
+++ assets/js/ytscript.js
@@ -0,0 +1,20 @@
+
+var ytAPI = "AIzaSyB1wxbpiJoa90aLYGo-ZJOoRmCGQEuM2jY"
+var searchedPlantName = "rose"
+var plantNameSearched = `%22${searchedPlantName}+care%22`
+
+
+fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=${plantNameSearched}&key=${ytAPI}`)
+.then((ytResults)=>{
+    return ytResults.json()
+}).then((data)=>{
+    console.log(data)
+    let videos = data.items
+    let videoContainer = document.querySelector(".ytContainer")
+    for(video of videos){
+        videoContainer.innerHTML += `
+            <img src="${video.snippet.thumbnails.default.url}">
+        `
+    }
+})
+
